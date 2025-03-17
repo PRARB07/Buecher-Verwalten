660866df5cbb9786efe5aa3d5d34c343cf062212
Projekt123456 <projekt.wv79z@simplelogin.fr>
2025-03-17 09:57:02 +0100

    Add MYSQL Script

---
+++ script.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE Buecher_Verwalten;
+
+CREATE TABLE Genre (
+    GenreID INT NOT NULL,
+    Genre VARCHAR(50),
+    PRIMARY KEY (GenreID)
+);
+
+CREATE TABLE Kunde (
+    KundenID INT NOT NULL, 
+    Vorname VARCHAR(50),
+    Nachname VARCHAR(50),
+    Ort VARCHAR(50),
+    Straße VARCHAR(50),
+    PLZ VARCHAR(5),
+    PRIMARY KEY (KundenID)
+);
+
+CREATE TABLE Ausleihung (
+    AusleihungID INT NOT NULL,
+    Ausleihdatum DATETIME,
+    KundenID INT,
+    PRIMARY KEY (AusleihungID),
+    FOREIGN KEY (KundenID) REFERENCES Kunde(KundenID)
+);
+
+CREATE TABLE Buch (
+    BuchID INT NOT NULL,
+    Titel VARCHAR(255),
+    Autor VARCHAR(255),
+    Erscheinungsdatum DATE,
+    Beschreibung TEXT,
+    Genre INT,
+    AusleihungID INT,
+    PRIMARY KEY (BuchID),
+    FOREIGN KEY (AusleihungID) REFERENCES Ausleihung(AusleihungID)
+);
+
+CREATE TABLE Genre2Buch (
+    BuchID INT,
+    GenreID INT,
+    FOREIGN KEY (BuchID) REFERENCES Buch(BuchID),
+    FOREIGN KEY (GenreID) REFERENCES Genre(GenreID)
+);
